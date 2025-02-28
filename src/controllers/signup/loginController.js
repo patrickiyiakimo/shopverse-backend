629aac46014294d3d213964c08d8c0433bb902c3
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-02-28 11:59:25 +0100

    added a new folder

--- src/controllers/signup/loginController.js
+++ src/controllers/signup/loginController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const pool = require("../config/db");
+const pool = require("../../config/db");
 require("dotenv").config();
 
 
