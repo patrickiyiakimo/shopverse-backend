c51686e763ee0046271bace7ab166e7aefe525c8
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-03-04 11:03:29 +0100

    moved the login routes to its folder

--- src/routes/signup/login.js
+++ src/routes/signup/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../config/db");
+const pool = require("../../config/db");
 const loginController = require("../controllers/loginController");
 
 router.post("/", loginController.loginUser);
