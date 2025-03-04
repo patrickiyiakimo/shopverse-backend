e0aa05cd1415b9dce4f32b359bf3d892279d99ff
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-03-04 11:04:35 +0100

    moved the register file to its folder route

--- src/routes/signup/register.js
+++ src/routes/signup/register.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../config/db");
+const pool = require("../../config/db");
 const registerController = require("../controllers/registerController");
 
 router.post("/", registerController.registerUser);
