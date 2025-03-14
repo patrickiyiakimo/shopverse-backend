e712edc8154d803fe0e9ed337ddf6170db3b07ce
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-03-14 17:49:00 +0100

    feat: changed the status message

--- src/controllers/signup/registerController.js
+++ src/controllers/signup/registerController.js
@@ -7,7 +7,7 @@ const registerUser = async (req, res) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-        return res.status(400).json({ message: 'Username, email and password are required' });
+        return res.status(400).json({ message: 'All fields are required' });
     }
 
     try {
