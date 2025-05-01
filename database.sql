095f1ef59460bd413a22368b603cac83b834712a
Patrick Iyiakimo <137924636+patrickiyiakimo@users.noreply.github.com>
2025-05-02 00:09:47 +0100

    feat: created my shopverse database and the user table

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE shopverse;
+
+CREATE TABLE user(
+    user_id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+)
