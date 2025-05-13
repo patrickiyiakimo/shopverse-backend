b6f409264dcf16d4c58b5e19cf3b0b97795535a4
Patrick Iyiakimo <137924636+patrickiyiakimo@users.noreply.github.com>
2025-05-13 10:26:34 +0100

    feat: create the DELETE endpoint for each products

--- src/controllers/product/productController.js
+++ src/controllers/product/productController.js
@@ -38,3 +38,35 @@ exports.createProduct = async (req,res) => {
         res.sendStatus(500).json({message: "Internal server error"})
     }
 }
+
+exports.updateProduct = async (req, res) => {
+    const {id} = req.params;
+    const {name, description, price, image_url, stock} = req.body;
+    if (!name || !description || !price || !image_url || !stock) {
+        return res.status(400).json({message: "All fields are required"});
+    }
+    try {
+        const result = await db.query("UPDATE products SET name = $1, description = $2, price = $3, image_url = $4, stock = $5 WHERE id = $6 RETURNING *", [name, description, price, image_url, stock, id]);
+        if (result.rows.length === 0){
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500).json({message: "Internal server error"})
+    }
+};
+
+exports.deleteProduct = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const result = await db.query("DELETE FROM products WHERE id = $1 RETURNING *", [id]);
+        if (result.rows.length === 0){
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json({message: "Product deleted successfully"});
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500).json({message: "Internal server error"})
+    }
+}
